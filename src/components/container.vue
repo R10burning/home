23de5f42d39683d1164b24264d5aafd774201b4d
dengxiang <790309793@qq.com>
2018-03-07 16:06:07 +0800

    init

---
+++ src/components/container.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{listData}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      listData:'dasdsads',
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {
+    handleClick () {
+      alert(2);
+    }
+  }
+}
+
+</script>
+<style lang='less' scoped>
+</style>
